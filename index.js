cdef75252f9ca74349e8acaee1acd20e742c7e4a
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-12-18 14:34:11 -0500

    working on router

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 5050;
+const posts = require('./routes/posts');
+const connectToDatabase = require('./db/conn');
+connectToDatabase();
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port: ${PORT}`)
+app.use('/api/posts', posts);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
